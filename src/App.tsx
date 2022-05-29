ac0fed6657a0afe63c81d7790dea95b9a7ece118
hamdikaan <hkaan404@gmail.com>
2022-05-29 22:48:07 +0300

    Setup file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.scss";
+import { Clock, Stopwatch, Timer } from "./components";
 
 const App = () => {
-
   return (
     <div className="app">
-        <h1>React-Clock</h1>
+      <h1>React-Clock</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
