7096d443eac3c7e96ed2be3239b01f51ff36f4cf
hamdikaan <hkaan404@gmail.com>
2022-06-12 12:08:03 +0300

    Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import "./App.scss";
 import { Clock, Stopwatch, Timer } from "./components";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="app">
-      <Routes>
-        <Route index element={<Clock />} />
-        <Route path="/stopwatch" element={<Stopwatch />} />
-        <Route path="/timer" element={<Timer />} />
-      </Routes>
+      <div className="panel">
+        <div className="navbar">
+          <Link to="/">clock</Link>
+          <Link to="/stopwatch">stopwatch</Link>
+          <Link to="/timer">timer</Link>
+          <Link to="/">
+            <IoSettingsOutline />
+          </Link>
+        </div>
+        <Routes>
+          <Route index element={<Clock />} />
+          <Route path="/stopwatch" element={<Stopwatch />} />
+          <Route path="/timer" element={<Timer />} />
+        </Routes>
+      </div>
     </div>
   );
 };
